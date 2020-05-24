ee21d9e5dadba7eb95673af0f4b6bdae8ad5e221
craigatk <atk.craig@gmail.com>
2020-05-24 14:55:28 -0500

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# projektor-action
+# Projektor GitHub action
+
+GitHub action for publishing test results to Projektor for easy viewing in the Projektor UI.
+
+## Example usage
+
+```
+uses: craigatk/projektor-action@v1
+with:
+  config-file: projektor.json
+```
---
+++ index.js
@@ -0,0 +1,27 @@
+const core = require('@actions/core');
+const { run } = require('projektor-publish')
+
+const execute = async () => {
+    try {
+        const configFilePath = core.getInput('config-file');
+        const resultsInput = core.getInput('results')
+        const attachmentsInput = core.getInput('attachmentsInput')
+
+        const results = resultsInput ? resultsInput.split(/\r?\n/) : null;
+        const attachments = attachmentsInput ? resultsInput.split(/\r?\n/) : null;
+
+        const args = []
+
+        if (configFilePath) {
+            args.push(`--configFile=${configFilePath}`)
+        }
+
+        const {reportUrl} = await run([], null, 'projektor.json')
+
+        core.setOutput("report-url", reportUrl);
+    } catch (error) {
+        core.setFailed(error.message);
+    }
+}
+
+execute()
