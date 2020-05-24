026ce71f03c3baa48420fd4ff5c6d77318ba4a47
craigatk <atk.craig@gmail.com>
2020-05-24 15:08:17 -0500

    Building single Node script

--- index.js
+++ index.js
@@ -1,27 +1,27 @@
-const core = require('@actions/core');
-const { run } = require('projektor-publish')
+const core = require("@actions/core");
+const { run } = require("projektor-publish");
 
 const execute = async () => {
-    try {
-        const configFilePath = core.getInput('config-file');
-        const resultsInput = core.getInput('results')
-        const attachmentsInput = core.getInput('attachmentsInput')
+  try {
+    const configFilePath = core.getInput("config-file");
+    const resultsInput = core.getInput("results");
+    const attachmentsInput = core.getInput("attachmentsInput");
 
-        const results = resultsInput ? resultsInput.split(/\r?\n/) : null;
-        const attachments = attachmentsInput ? resultsInput.split(/\r?\n/) : null;
+    const results = resultsInput ? resultsInput.split(/\r?\n/) : null;
+    const attachments = attachmentsInput ? resultsInput.split(/\r?\n/) : null;
 
-        const args = []
+    const args = [];
 
-        if (configFilePath) {
-            args.push(`--configFile=${configFilePath}`)
-        }
-
-        const {reportUrl} = await run([], null, 'projektor.json')
-
-        core.setOutput("report-url", reportUrl);
-    } catch (error) {
-        core.setFailed(error.message);
+    if (configFilePath) {
+      args.push(`--configFile=${configFilePath}`);
     }
-}
 
-execute()
+    const { reportUrl } = await run(args, null, "projektor.json");
+
+    core.setOutput("report-url", reportUrl);
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+};
+
+execute();
